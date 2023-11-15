e322afdb69296b86a2a39a9d9a3ebc18df670533
AndreyChuck <a.chucklanov@yandex.ru>
2023-11-15 19:24:19 +0400

    добавлено перечисление "дни недели"

--- ConsoleApp3.4.3/Program.cs
+++ ConsoleApp3.4.3/Program.cs
@@ -6,4 +6,15 @@
         Yellow = 200,
         Green = 300
     }
+    enum DaysOfWeek : byte
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7
+    }
+
 }
